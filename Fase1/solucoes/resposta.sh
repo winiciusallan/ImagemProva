3c3756ade0fa039c02f384c09aec88227062f841
winiciusallan <winiciusab12@gmail.com>
2023-05-10 19:40:16 -0300

    adicionar useradm grupo

--- Fase1/solucoes/resposta.sh
+++ Fase1/solucoes/resposta.sh
@@ -4,6 +4,8 @@
 
 # O comando groupadd serve para a criação de um novo grupo no sistema.
 # O parâmetro -g passado permite escolher o GID (Group ID) seguido do nome que será criado
+groupadd -g 1010 josegota # Grupo do administrador
+
 groupadd -g 1000 administracao
 groupadd -g 2000 enfermeiros
 groupadd -g 3000 medicos
@@ -17,7 +19,7 @@ log_archive="/var/log/createusers.log" # Arquivo onde será gerado o log.
 # O passwd é um comando para alterar/setar a senha de algum usuário.
 # A flag -d deleta a senha tornando ela vazia. Não precisa de autenticação para o login.
 # A flag -e expira a senha do usuário e faz com que na proxima vez que ele se logue, precise mudar ela.
-useradd -u 1000 -G administracao,enfermeiros,medicos josegota >> $log_archive
+useradd -u 1000 -g 1010 -G administracao,enfermeiros,medicos josegota >> $log_archive
 passwd -d -e josegota | >> $log_archive
 
 for (( i=1 ; i<=15 ; i++ )) do
